8ed6758fa1dbe735f280938f2a34f77257084712
Harsheyy <harshkdesai109@yahoo.com>
2025-07-12 18:00:51 -0400

    analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "@/components/ui/provider";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.scss'
 import App from './App.tsx'
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Provider>
         <App />
+        <Analytics />
       </Provider>
   </StrictMode>
 )
